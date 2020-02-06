f207e3d9ceb7edbb63a31da400550e63aae9b2d0
catarina <c.alexandracorreia@ua.pt>
2020-02-06 15:28:28 +0000

    Update random forest classifier

--- randomForest/main.py
+++ randomForest/main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 from randomForest import single_decision_tree, limit_max_depth
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
 def no_maximumDepth(rseed, x, y):
@@ -13,10 +16,26 @@ def maximunDepth(rseed, max_depth, x, y):
     print(f'Decision tree has {tree.tree_.node_count} nodes with maximum depth {tree.tree_.max_depth}.')
     print(f'Model Accuracy: {tree.score(x, y)}')
 
-def with_dataset(path, rseed, type):
-    data = pd.read_csv(path).sample(10, random_state = rseed)
-    data = data.select_dtypes(type)
-    #Label distribution ??
+def with_dataset(path):
+    #Assign column names to the dataset
+    headernames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+    #read dataset to pandas dataframe
+    dataset = pd.read_csv(path, names = headernames)
+    dataset.head()
+    # .iloc[] is primarily integer position based (from 0 to length-1 of the axis), but may also be used with a boolean array
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, 4].values
+    # divide the data into train and test split: split the dataset into 70% training data and 30% of testing data −
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)
+    # train the model
+    classifier = RandomForestClassifier(n_estimators = 50)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    result = confusion_matrix(y_test, y_pred)
+    result1 = classification_report(y_test, y_pred)
+    result2 = accuracy_score(y_test,y_pred)
+    return result, result1, result2
+    
 
 def main():
     #set random seed to ensure reproductible runs
@@ -26,6 +45,12 @@ def main():
     max_depth = 2
     no_maximumDepth(rseed, x, y)
     maximunDepth(rseed, max_depth, x, y)
-    with_dataset('dataset.csv', rseed, 'number')
+    path = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    result, result1, result2 =  with_dataset(path)
+    print("Confusion Matrix:")
+    print(result)
+    print("Classification Report:",)
+    print (result1)
+    print("Accuracy:",result2)
 
 main()
